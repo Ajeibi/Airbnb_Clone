580ffbcd4c618784f410cdad1ca40a143db01733
Ajeibi <ajeibipikin@gmail.com>
2023-08-15 13:10:10 +0100

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+
+        <div className="footer">
+        <p>© 2020 Airbnb clone! No right reserved - this is a demo !!</p>    
+        <p>oche</p>
+        </div>
+    )
+}
+
+export default Footer
